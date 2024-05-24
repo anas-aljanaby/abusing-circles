c8ecbb631db006d8ff880911d422904eae288d90
anas-aljanaby <anas.aljanaby00@gmail.com>
2024-05-24 05:21:48 +0300

    optimize handle_event and container adding

--- game.py
+++ game.py
@@ -7,28 +7,30 @@ import time
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
-CONT_WIDTH = SCREEN_WIDTH / 2
-CONT_HEIGHT = SCREEN_HEIGHT / 2
 MIN_SPEED_FOR_ROTATION = 10
 ROTATION_PROBABILITY = 0.9
 MIN_ROTATION_ANGLE = 5
 MAX_ROTATION_ANGLE = 15
-
+SCREEN_LEFT_BOUND = -100  
+SCREEN_RIGHT_BOUND = SCREEN_WIDTH + 100
+SCREEN_TOP_BOUND = -100 
+SCREEN_BOTTOM_BOUND = SCREEN_HEIGHT + 100
+ORB_X_MIN = 500
+ORB_X_MAX = 700
+ORB_Y_MIN = 300
+ORB_Y_MAX = 500
 
 class Game:
     def __init__(self, play_sound=False, radius=350, speed_boost=1.06,
-                 max_square_speed=1600):
+                 add_cont_interval=0.9, max_square_speed=1600):
         self.prev_cont = time.time()
         self.cur_cont = 0
         self.orb_out = False
         self.dynamic_cont = False
-        self.screen_width = 1200 
-        self.screen_height = 800
-        self.cont_width = self.screen_width / 2
-        self.cont_height = self.screen_height / 2 
+        self.cont_width = SCREEN_WIDTH/ 2
+        self.cont_height = SCREEN_HEIGHT / 2 
         self.radius = radius 
-        self.screen = pygame.display.set_mode((self.screen_width,
-                                               self.screen_height))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.container = CircleContainer(self.cont_width,
                                          self.cont_height, radius=self.radius)
         self.containers = [self.container]
@@ -40,6 +42,15 @@ class Game:
         self.increase_speed = False
         self.play_sound = play_sound 
         self.particles = []
+        self.add_cont_interval = add_cont_interval
+        
+        self.event_map = {
+            'play': self.play_game,
+            'reset': self.reset_game,
+            'size_increase': self.toggle_size_increase,
+            'speed_increase': self.toggle_speed_increase,
+            'toggle_container': self.toggle_container,
+            }
         if self.play_sound:
             mixer.init()
             self.sound = pygame.mixer.Sound('ball_hit.mp3')
@@ -88,26 +99,26 @@ class Game:
 
     def boost_orb_speed(self):
         y_speed = self.orb.y_speed * self.speed_boost 
-        if (self.orb.x_speed ** 2 + self.orb.y_speed ** 2) < self.max_square_speed:
+        if (self.orb.x_speed ** 2 + y_speed ** 2) < self.max_square_speed:
             self.orb.y_speed = y_speed
 
     def grow_orb(self, cont):
         if self.orb.radius >= cont.radius:
             time.sleep(2)
-            self.create_orb(running=True)
+            self.reset_game(running=True)
         else:
             self.orb.radius += 1
 
-    def is_orb_out(self):
-        if self.containers:
-            cont = self.containers[-1]
-        else:
-            cont = CircleContainer(self.cont_width, self.cont_height, radius=self.radius)
-
-        squared_dist = self.square_dist_to_container(cont)
-        if squared_dist >= (cont.radius + self.orb.radius)**2:
+    def orb_out_container(self):
+        squared_dist = self.square_dist_to_container(self.container)
+        if squared_dist >= (self.container.radius + self.orb.radius)**2:
             self.orb_out = True
-        if squared_dist >= 700_000: #(cont.radius + self.orb.radius)**2: # - self.orb.radius)**2 + 10000:
+    
+    def orb_out_screen(self):
+        orb_x_out_of_bounds = self.orb.x > SCREEN_RIGHT_BOUND or self.orb.x < SCREEN_LEFT_BOUND
+        orb_y_out_of_bounds = self.orb.y > SCREEN_BOTTOM_BOUND or self.orb.y < SCREEN_TOP_BOUND
+
+        if orb_x_out_of_bounds or orb_y_out_of_bounds:
             self.running = False
 
     def rotate_vector(self, vx, vy, angle):
@@ -119,20 +130,19 @@ class Game:
         return new_vx, new_vy
 
     def speed_in_bounds(self, x_speed, y_speed):
-        speed = math.sqrt(x_speed**2 + y_speed**2)
-        return speed < 40 
+        speed = x_speed**2 + y_speed**2
+        return speed < self.max_square_speed
 
     def create_orb(self, running=False):
         self.running = running
-        return Orb(random.randint(500, 700), random.randint(300, 500))
+        return Orb(random.randint(ORB_X_MIN, ORB_X_MAX),
+                   random.randint(ORB_Y_MIN, ORB_Y_MAX))
 
     def play_game(self):
         self.running = True
 
     def reset_game(self, running=False):
         self.orb = self.create_orb(running=running)
-        self.containers = []
-        self.container = CircleContainer(self.cont_width, self.cont_height, radius=self.radius)
         self.containers = [self.container]
         self.orb_out = False
 
@@ -145,27 +155,22 @@ class Game:
             self.increase_speed = not self.increase_speed
 
     def toggle_container(self):
-        self.dynamic_cont = not self.dynamic_cont
+        if not self.running:
+            self.dynamic_cont = not self.dynamic_cont
 
     def handle_event(self, event):
-        if event.lower() == 'play':
-            self.play_game()
-        elif event.lower() == 'reset':
-            self.reset_game()
-        elif event == 'size_increase':
-            self.toggle_size_increase() 
-        elif event == 'speed_increase':
-            self.toggle_speed_increase()
-        elif event == 'toggle_container':
-            self.toggle_container()
+        if event in self.event_map:
+            self.event_map[event]()
 
     def update_container(self):
         if self.dynamic_cont and not self.orb_out:
-            if  time.time() - self.prev_cont > 0.7:
-                try:
-                    self.containers.insert(0, CircleContainer(self.cont_width, self.cont_height, radius=self.containers[0].radius-2))
-                except IndexError:
-                    self.containers.insert(0, CircleContainer(self.cont_width, self.cont_height, radius=self.radius))
+            if  time.time() - self.prev_cont > self.add_cont_interval:
+                if self.containers:
+                    self.containers.insert(0, CircleContainer(
+                        self.cont_width, self.cont_height,
+                        radius=self.containers[0].radius-2))
+                else:
+                    self.containers = [self.container]
                 self.prev_cont = time.time()
 
     def draw(self):
@@ -193,7 +198,8 @@ class Game:
     def update(self):
         self.update_container()
         self.orb.update()
-        self.is_orb_out()
+        self.orb_out_container()
+        self.orb_out_screen()
         self.check_collision()
         self.update_particles()
 
